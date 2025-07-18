63ec87ba9e9d6f1ea2dd007f3181c3bb8a9ac180
esuglia <esuglia@gmail.com>
2025-07-18 14:59:04 -0700

    data viz and registrant data edits

--- 2025_benthic_guidance_trainings_data_viz.R
+++ 2025_benthic_guidance_trainings_data_viz.R
@@ -8,9 +8,10 @@ library(dplyr)
 
 # Load data ----
 participants = read.csv("2025-05_benthic_guidance_training_participant_data.csv")
+# cleaned up registrant list manually in excel
 d = read.csv("2025-05_benthic_guidance_training_registrant_data.csv") %>%
   left_join(participants) %>%
-  mutate(attended = ifelse(is.na(guest)==TRUE, "No", "Yes"))
+  mutate(attended = ifelse(is.na(guest) == TRUE, "No", "Yes"))
 
 # Add column with counts for each org
 d_counts = d %>%
@@ -27,6 +28,24 @@ d_counts %>%
 # Plot by org, participants
 d_counts %>%
   filter(attended == "Yes") %>%
+  ggplot(aes(org, count)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  theme_classic()
+
+# make a for loop to plot participant numbers by organization category
+# State Government shown here:
+d_counts %>%
+  filter(attended == "Yes") %>%
+  filter(org_category == "State_Government") %>%
+  ggplot(aes(org, count)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  theme_classic()
+
+d_counts %>%
+  filter(attended == "Yes") %>%
+  filter(org_category == "Tribe") %>%
   ggplot(aes(org, count)) +
   geom_bar(stat = "identity") +
   coord_flip() +
@@ -58,4 +77,3 @@ d_categories %>%
 # Generate a list of organizations
 orgs = unique(d_counts$org)
 orgs
-
