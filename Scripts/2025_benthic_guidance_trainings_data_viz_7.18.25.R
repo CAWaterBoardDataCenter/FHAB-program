6036d89af6ad597aa873a93f5a68712b8bcb3f2d
esuglia <esuglia@gmail.com>
2025-07-28 16:52:56 -0700

    updated maps

--- Scripts/2025_benthic_guidance_trainings_data_viz_7.18.25.R
+++ Scripts/2025_benthic_guidance_trainings_data_viz_7.18.25.R
@@ -24,7 +24,7 @@ participants_w_orgs = participants %>%
 #populate manually
 
 # read in dataframe with organization location info
-org_loc = read.csv("data/org_loc.csv")
+org_loc_latlong = read.csv("data/org_loc_latlong.csv")
 
 ### Making a map showing participants by county & organization ###
 # want to color counties in a gradient so that darker shades represent more participants for that county
@@ -39,20 +39,18 @@ n_org_cat = participants_w_orgs %>%
 
 # TO DO: one instance of USFS is labeled as state govt; should be federal govt
 
-n_cty = org_loc %>%
+n_cty = org_loc_latlong %>%
   group_by(county) %>%
   summarize(n_cty = n()) %>%
   #rename counties so they match ca_counties df (all lowercase)
   mutate(county = tolower(county))
 
 # make a df with number of participants by county
-all_dat = org_loc %>%
-  select(org, county) %>%
+all_dat = org_loc_latlong %>%
+  select(org, county, lat, long) %>%
   left_join(n_cty) %>%
   left_join(n_org) %>%
   left_join(n_org_cat)
-
-#CNRA?: 38.57538259282173, -121.49935629279456
 
 # load county data
 usa_counties = map_data("county") 
@@ -71,16 +69,42 @@ ca_counties = usa_counties %>%
 #library(RColorBrewer)
 
 teal_gradient = c("white", "#d1eeea","#a8dbd9", "#85c4c9", "#68abb8", "#4f90a6", "#3b738f", "#2a5674")
+library(ggrepel)
 
 # Create the map with counties
 ggplot(data = ca_counties, aes(x = long, y = lat, group = group)) +
   geom_polygon(aes(fill = n_cty), color = "black",  linewidth = 0.2) + # County boundaries # #
   scale_fill_gradientn(colors = teal_gradient) +
+  geom_point(data = all_dat %>%
+               # remove counties outside CA for now
+               drop_na(county), aes(x = long, y = lat, group = org, size = n_org, shape = org_category), color = "orange") + #shape = 1, 
+  geom_label_repel(data = all_dat %>%
+                filter(county != "Sacramento") %>%
+               # remove counties outside CA for now
+               drop_na(county), aes(x = long, y = lat, group = org, label = org), nudge_x = 0.2, box.padding = 1.3) +
   coord_fixed(1.3) +  # Adjust aspect ratio if needed
   theme_void() +
-  labs(title = "Counties of California")
+  labs(title = "Participants by Organization & County")
 
 # Add points using lat/longs for orgs, and size points by how many participants were from each org
+
+# Only Sacramento county to use as a pop-out
+ggplot(data = ca_counties %>% filter(subregion == "sacramento"), aes(x = long, y = lat, group = group)) +
+  geom_polygon(fill = "#2a5674", color = "black",  linewidth = 0.2) + # County boundaries # #
+  scale_fill_gradientn(colors = teal_gradient) +
+  geom_point(data = all_dat %>%
+               filter(county == "Sacramento") %>%
+               # remove counties outside CA for now
+               drop_na(county),
+               aes(x = long, y = lat, group = org, size = n_org), shape = 7, color = "orange") +
+  geom_label_repel(data = all_dat %>%
+                     filter(county == "Sacramento") %>%
+                     # remove counties outside CA for now
+                     drop_na(county),
+                   aes(x = long, y = lat, group = org, label = org), nudge_x = 0.2, box.padding = 1.3) +
+  coord_fixed(1.3) +  # Adjust aspect ratio if needed
+  theme_void() +
+  labs(title = "Sacramento County")
 
 # If we wanted to replicate the above with regional WB boundaries, would have to import a shp file online using sf package and then map
 
