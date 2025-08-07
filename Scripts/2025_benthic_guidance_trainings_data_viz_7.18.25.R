89a86e4f68ad1e18bdac3cc8458241421358276a
Elena Suglia <esuglia@gmail.com>
2025-08-07 15:17:55 -0700

    updated data viz script

--- Scripts/2025_benthic_guidance_trainings_data_viz_7.18.25.R
+++ Scripts/2025_benthic_guidance_trainings_data_viz_7.18.25.R
@@ -71,61 +71,61 @@ ca_counties = usa_counties %>%
 teal_gradient = c("white", "#d1eeea","#a8dbd9", "#85c4c9", "#68abb8", "#4f90a6", "#3b738f", "#2a5674")
 library(ggrepel)
 
-# Create the map with counties
+## TO DO: Add state column, fill out county column completely
+
+#### Create the map with counties ####
 ggplot(data = ca_counties, aes(x = long, y = lat, group = group)) +
   geom_polygon(aes(fill = n_cty), color = "black",  linewidth = 0.2) + # County boundaries # #
   scale_fill_gradientn(colors = teal_gradient) +
   geom_point(data = all_dat %>%
+               filter(state == "CA"),
                # remove counties outside CA for now
-               drop_na(county), aes(x = long, y = lat, group = org, size = n_org, shape = org_category), color = "orange") + #shape = 1, 
+               aes(x = long, y = lat, group = org, size = n_org, shape = org_category), color = "orange") + #shape = 1, 
   geom_label_repel(data = all_dat %>%
                 filter(county != "Sacramento") %>%
                # remove counties outside CA for now
-               drop_na(county), aes(x = long, y = lat, group = org, label = org), nudge_x = 0.2, box.padding = 1.3) +
+               drop_na(county), aes(x = long, y = lat, group = org, label = org), nudge_x = 0.2, box.padding = 1.3, max.overlaps = Inf) + # make sure all points are labeled
   coord_fixed(1.3) +  # Adjust aspect ratio if needed
   theme_void() +
   labs(title = "Participants by Organization & County")
 
-# Add points using lat/longs for orgs, and size points by how many participants were from each org
-
-# Only Sacramento county to use as a pop-out
+#### Only Sacramento county to use as a pop-out ####
 ggplot(data = ca_counties %>% filter(subregion == "sacramento"), aes(x = long, y = lat, group = group)) +
   geom_polygon(fill = "#2a5674", color = "black",  linewidth = 0.2) + # County boundaries # #
   scale_fill_gradientn(colors = teal_gradient) +
   geom_point(data = all_dat %>%
-               filter(county == "Sacramento") %>%
+               filter(county == "Sacramento"),
                # remove counties outside CA for now
-               drop_na(county),
                aes(x = long, y = lat, group = org, size = n_org), shape = 7, color = "orange") +
   geom_label_repel(data = all_dat %>%
                      filter(county == "Sacramento") %>%
                      # remove counties outside CA for now
                      drop_na(county),
-                   aes(x = long, y = lat, group = org, label = org), nudge_x = 0.2, box.padding = 1.3) +
+                   aes(x = long, y = lat, group = org, label = org), nudge_x = 0.2, box.padding = 1.3, max.overlaps = Inf) +
   coord_fixed(1.3) +  # Adjust aspect ratio if needed
   theme_void() +
   labs(title = "Sacramento County")
 
 # If we wanted to replicate the above with regional WB boundaries, would have to import a shp file online using sf package and then map
 
-# Code from pop_map in PhD work ----
-# Subset map data for only California
-ca_df = subset(usa_states,region=='california') 
-ca_map = ggplot(ca_df) + # create base map of California
-  geom_polygon(aes(x=long,y=lat,group=group),color='white',fill='grey') +
-  #coord_fixed(1.3,ylim=c(36.6,40.6),xlim=c(-124.1,-118.5)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_blank(), axis.title = element_blank(),axis.text=element_blank(),axis.ticks = element_blank())
-ca_map
+library(stringr)
+#### Create a bar graph depicting non-WB state agency representation ####
+all_dat %>%
+  filter(org_category == "State_Government") %>%
+  filter(!str_detect(org, "Water Board")) %>%
+  filter (state == "CA") %>%
+  ggplot(aes(org, n_org)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  theme_classic()
 
-#ggsave("~/GitHub/pop-gen/figures/pop_map.png")
-
-# Build map coloring pops by elevation
-pop_map <- ca_map +
-  geom_point(data=org_loc,aes(y=lat,x=long,color=el),size=5) + #, size=3, width = 0.2, height = 0.05
-  scale_color_continuous(low='orange',high='blue',name='Elevation (m)',guide="none") +
-  #geom_text_repel(data=site_info,aes(x=long,y=lat,label=pop),box.padding=.9)
-  geom_label_repel(data=org_loc, aes(x=long,y=lat, label = pop), box.padding = 0.1, nudge_x = 0.35) 
-pop_map
+#### Create a bar graph showing numbers of participants from outside CA ####
+all_dat %>%
+  filter(state != "CA") %>%
+  ggplot(aes(org, n_org)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  theme_classic()
 
 # Outdated code for plotting bar charts ----
 # Add column with counts for each org
